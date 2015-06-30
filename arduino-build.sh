0e3a9c18f6442f4ae51da7822e4eb52fedd61692
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-06-29 17:20:15 -0700

    Zip it up and release it

--- arduino-build.sh
+++ arduino-build.sh
@@ -4,10 +4,10 @@ TOOLS_DIR=$BASE_DIR/tools
 BUILD_DIR=$BASE_DIR/build
 SRC_DIR=$BASE_DIR/build/src
 
-# echo "Clean"
-#
-# rm -rf $BASE_DIR/build
-# mkdir -p $BUILD_DIR
+echo "Clean"
+
+rm -rf $BASE_DIR/build
+mkdir -p $BUILD_DIR
 
 echo "Copy h & cpp"
 
@@ -16,30 +16,30 @@ cp library.properties $BUILD_DIR/
 cp tentacle-build.h $SRC_DIR/
 cp tentacle-build.cpp $SRC_DIR/
 
-# echo "Installing tools"
-#
-# rm -rf $TOOLS_DIR
-# mkdir $TOOLS_DIR
-#
-# mkdir -p $TOOLS_DIR/nanopb
-# curl -sL http://koti.kapsi.fi/~jpa/nanopb/download/nanopb-0.3.3-macosx-x86.tar.gz | tar xz -C $TOOLS_DIR/nanopb --strip-components 1
-#
-# echo "Downloading projects"
-#
-# mkdir -p $SRC_DIR/arduino-nanopb
-# curl -sL https://github.com/octoblu/arduino-nanopb/archive/master.tar.gz | tar xz -C $SRC_DIR/arduino-nanopb --strip-components 1
-#
-# mkdir -p $SRC_DIR/tentacle
-# curl -sL https://github.com/octoblu/tentacle/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle --strip-components 1
-#
-# mkdir -p $SRC_DIR/tentacle-arduino
-# curl -sL https://github.com/octoblu/tentacle-arduino/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-arduino --strip-components 1
-#
-# mkdir -p $SRC_DIR/tentacle-pseudopod
-# curl -sL https://github.com/octoblu/tentacle-pseudopod/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-pseudopod --strip-components 1
-#
-# mkdir -p $SRC_DIR/tentacle-protocol-buffer
-# curl -sL https://github.com/octoblu/tentacle-protocol-buffer/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-protocol-buffer --strip-components 1
+echo "Installing tools"
+
+rm -rf $TOOLS_DIR
+mkdir $TOOLS_DIR
+
+mkdir -p $TOOLS_DIR/nanopb
+curl -sL http://koti.kapsi.fi/~jpa/nanopb/download/nanopb-0.3.3-linux-x86.tar.gz | tar xz -C $TOOLS_DIR/nanopb --strip-components 1
+
+echo "Downloading projects"
+
+mkdir -p $SRC_DIR/arduino-nanopb
+curl -sL https://github.com/octoblu/arduino-nanopb/archive/master.tar.gz | tar xz -C $SRC_DIR/arduino-nanopb --strip-components 1
+
+mkdir -p $SRC_DIR/tentacle
+curl -sL https://github.com/octoblu/tentacle/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle --strip-components 1
+
+mkdir -p $SRC_DIR/tentacle-arduino
+curl -sL https://github.com/octoblu/tentacle-arduino/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-arduino --strip-components 1
+
+mkdir -p $SRC_DIR/tentacle-pseudopod
+curl -sL https://github.com/octoblu/tentacle-pseudopod/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-pseudopod --strip-components 1
+
+mkdir -p $SRC_DIR/tentacle-protocol-buffer
+curl -sL https://github.com/octoblu/tentacle-protocol-buffer/archive/master.tar.gz | tar xz -C $SRC_DIR/tentacle-protocol-buffer --strip-components 1
 
 echo "Building tentacle-pseudopod"
 cd $SRC_DIR/tentacle-pseudopod

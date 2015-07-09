65ef14fd0d200c181e1062861ff053a5c1028097
Travis CI <sqrt+travis@octoblu.com>
2015-07-08 17:11:02 -0700

    particle deploy in progress

--- particle/build.sh
+++ particle/build.sh
@@ -15,11 +15,11 @@ mkdir -p $SRC_DIR
 
 echo "copying main headers"
 cp $PLATFORM/spark.json $BUILD_DIR/
-cp $PLATFORM/tentacle-build.h $SRC_DIR/
-cp $PLATFORM/tentacle-build.cpp $SRC_DIR/
+cp $PLATFORM/tentacle-particle.h $SRC_DIR/
+cp $PLATFORM/tentacle-particle.cpp $SRC_DIR/
 
 echo "copying examples"
-cp -r $PLATFORM/examples $BUILD_DIR/examples
+cp -r $PLATFORM/examples $SRC_DIR/examples
 
 echo "copying projects"
 cp -r $DOWNLOAD_DIR/* $SRC_DIR
@@ -29,3 +29,14 @@ cd $SRC_DIR/tentacle-pseudopod
 cp $SRC_DIR/tentacle-protocol-buffer/tentacle-message.proto .
 rm -f *.pb*
 $TOOLS_DIR/nanopb/generator-bin/protoc --nanopb_out=. tentacle-message.proto
+
+cd $BASE_DIR
+echo "'Fixing' library paths"
+sed -ine 's/"tentacle.h"/"tentacle-particle\/tentacle\/tentacle.h"/' $SRC_DIR/tentacle-arduino/tentacle-arduino.h
+sed -ine 's/"proto-buf.h"/"tentacle-particle\/tentacle-pseudopod\/proto-buf.h"/' $SRC_DIR/tentacle/tentacle.h
+sed -ine 's/<pb.h>/"arduino-nanopb\/pb.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-message.pb.h
+sed -ine 's/"pb_encode.h"/"tentacle-particle\/arduino-nanopb\/pb_encode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
+sed -ine 's/"pb_decode.h"/"tentacle-particle\/arduino-nanopb\/pb_decode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
+sed -ine 's/"pb_arduino_encode.h"/"tentacle-particle\/arduino-nanopb\/pb_arduino_encode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
+sed -ine 's/"pb_arduino_decode.h"/"tentacle-particle\/arduino-nanopb\/pb_arduino_decode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
+sed -ine 's/"tentacle.h"/"tentacle-particle\/tentacle\/tentacle.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
---
+++ particle/tentacle-particle.h
@@ -0,0 +1,2 @@
+#include "tentacle-particle/tentacle-arduino/tentacle-arduino.h"
+#include "tentacle-particle/tentacle-pseudopod/tentacle-pseudopod.h"

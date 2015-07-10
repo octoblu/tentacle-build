61769da24ca87dffb42e1f0c29e151b10703d725
Travis CI <sqrt+travis@octoblu.com>
2015-07-10 16:05:51 -0700

    travis, constructing the TERRIFYING TENTACLE for particle

--- particle/build.sh
+++ particle/build.sh
@@ -7,36 +7,53 @@ TOOLS_DIR=$BASE_DIR/tools
 BUILD_DIR=$BASE_DIR/build/$PLATFORM
 SRC_DIR=$BUILD_DIR/firmware
 
+DEPLOY_DIR=$BASE_DIR/deploy/$PLATFORM
+DEPLOY_SRC_DIR=$DEPLOY_DIR/firmware
+
 echo "building for $PLATFORM"
 echo "clean"
-rm -rf $BUILD_DIR
+# rm -rf $BUILD_DIR
+rm -rf $DEPLOY_DIR
 
 mkdir -p $SRC_DIR
+mkdir -p $DEPLOY_SRC_DIR
 
 echo "copying main headers"
-cp $PLATFORM/spark.json $BUILD_DIR/
+cp $PLATFORM/spark.json $DEPLOY_DIR/
 cp $PLATFORM/tentacle-particle.h $SRC_DIR/
 cp $PLATFORM/tentacle-particle.cpp $SRC_DIR/
 
-echo "copying examples"
-cp -r $PLATFORM/examples $SRC_DIR/examples
-
 echo "copying projects"
-cp -r $DOWNLOAD_DIR/* $SRC_DIR
+# cp -r $DOWNLOAD_DIR/* $SRC_DIR
 
 echo "Building tentacle-pseudopod"
-cd $SRC_DIR/tentacle-pseudopod
-cp $SRC_DIR/tentacle-protocol-buffer/tentacle-message.proto .
-rm -f *.pb*
-$TOOLS_DIR/nanopb/generator-bin/protoc --nanopb_out=. tentacle-message.proto
+# cd $SRC_DIR/tentacle-pseudopod
+# cp $SRC_DIR/tentacle-protocol-buffer/tentacle-message.proto .
+# rm -f *.pb*
+# $TOOLS_DIR/nanopb/generator-bin/protoc --nanopb_out=. tentacle-message.proto
+
+echo "cloning repo for $PLATFORM"
+echo git@github.com:octoblu/tentacle-dist-$PLATFORM
+git clone git@github.com:octoblu/tentacle-dist-$PLATFORM $DEPLOY_DIR
+git config user.name "Travis CI"
+git config user.email "sqrt+travis@octoblu.com"
+
+cd $DEPLOY_DIR
+rm -rf *.cpp *.h *.hpp *.hne examples/ firmware/ *tentacle* *arduino*
+
+mkdir -p $DEPLOY_SRC_DIR
 
 cd $BASE_DIR
-echo "'Fixing' library paths"
-sed -ine 's/"tentacle.h"/"tentacle-particle\/tentacle\/tentacle.h"/' $SRC_DIR/tentacle-arduino/tentacle-arduino.h
-sed -ine 's/"proto-buf.h"/"tentacle-particle\/tentacle-pseudopod\/proto-buf.h"/' $SRC_DIR/tentacle/tentacle.h
-sed -ine 's/<pb.h>/"arduino-nanopb\/pb.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-message.pb.h
-sed -ine 's/"pb_encode.h"/"tentacle-particle\/arduino-nanopb\/pb_encode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
-sed -ine 's/"pb_decode.h"/"tentacle-particle\/arduino-nanopb\/pb_decode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
-sed -ine 's/"pb_arduino_encode.h"/"tentacle-particle\/arduino-nanopb\/pb_arduino_encode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
-sed -ine 's/"pb_arduino_decode.h"/"tentacle-particle\/arduino-nanopb\/pb_arduino_decode.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
-sed -ine 's/"tentacle.h"/"tentacle-particle\/tentacle\/tentacle.h"/' $SRC_DIR/tentacle-pseudopod/tentacle-pseudopod.h
+echo "copying examples"
+cp -r $PLATFORM/examples $DEPLOY_SRC_DIR/examples
+
+cd $SRC_DIR
+find . -iname '*.c' -exec cp {} $DEPLOY_SRC_DIR/{}pp \; 2> /dev/null
+find . -iname '*.h' -exec cp {} $DEPLOY_SRC_DIR \; 2> /dev/null
+find . -iname '*.cpp' -exec cp {} $DEPLOY_SRC_DIR \; 2> /dev/null
+find . -iname '*.hpp'-exec cp {} $DEPLOY_SRC_DIR \; 2> /dev/null
+
+sed -ine 's/<pb.h>/"pb.h"/' $DEPLOY_SRC_DIR/tentacle-message.pb.h
+
+
+echo "copied new code"
--- particle/deploy.sh
+++ particle/deploy.sh
@@ -3,34 +3,9 @@ set -e
 PLATFORM=particle
 BASE_DIR=`pwd`
 DEPLOY_DIR=$BASE_DIR/deploy/$PLATFORM
-BUILD_DIR=$BASE_DIR/build/$PLATFORM
 
 echo "deploying for $PLATFORM"
-echo "clean"
-rm -rf $DEPLOY_DIR
-mkdir -p $DEPLOY_DIR
-
-echo "cloning repo for $PLATFORM"
-echo git@github.com:octoblu/tentacle-dist-$PLATFORM
-git clone git@github.com:octoblu/tentacle-dist-$PLATFORM $DEPLOY_DIR
-git config user.name "Travis CI"
-git config user.email "sqrt+travis@octoblu.com"
-
-echo "removing old code"
 cd $DEPLOY_DIR
-rm -rf *.cpp *.h examples/ firmware/ *tentacle* *arduino*
-cp -r $BUILD_DIR/* .
-echo "copied new code"
 git add .
 git commit -a -m "travis, constructing the TERRIFYING TENTACLE for $PLATFORM"
 git push
-
-
-# git add .
-# git commit -m "Deploy to GitHub Pages"
-#
-# # Force push from the current repo's master branch to the remote
-# # repo's gh-pages branch. (All previous history on the gh-pages branch
-# # will be lost, since we are overwriting it.) We redirect any output to
-# # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
